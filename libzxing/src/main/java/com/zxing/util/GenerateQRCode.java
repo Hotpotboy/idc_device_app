cf9c74a669e93ce8667abeb2c46edb88c4bf6660
HotpotBoy <407376249@qq.com>
2016-04-23 10:44:14 +0800

    主体界面大体完成

--- libzxing/src/main/java/com/zxing/util/GenerateQRCode.java
+++ libzxing/src/main/java/com/zxing/util/GenerateQRCode.java
@@ -4,8 +4,10 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,19 +23,53 @@ public class GenerateQRCode {
     /**project path*/
     public static final String PROJECTEC_PATH = System.getProperty("user.dir")+ File.separator+"libzxing";
     public static final int BITMAP_SIZE = 200;
+    /**以默认参数生成二维码*/
     public static void generatePNG(String content) throws WriterException, IOException {
-        int width = BITMAP_SIZE; // 图像宽度
-        int height = BITMAP_SIZE; // 图像高度
+        generateQRCODEForParams(ErrorCorrectionLevel.H,BITMAP_SIZE,0xff000000,0xffffffff,PROJECTEC_PATH+File.separator+content+".png",content);
+    }
+
+    /**
+     *
+     * @param erroCorrection   枚举值，排错率；分四个等级：L(7%)、M(15%)、Q(25%)、H(30%);
+     *                         排错率越高可存储的信息越少，但对二维码清晰度的要求越小
+     *                         （这个会影响二维码扫码的解析速度，排错率越高，解析速度越快）
+     * @param size             尺寸大小
+     * @param bgColor          背景色,16进制颜色值
+     * @param fgColor          二维码颜色,16进制颜色值
+     * @param savedPath        保存路径
+     * @param content          二维码信息
+     */
+    public static void generateQRCODEForParams(ErrorCorrectionLevel erroCorrection,int size,int bgColor,int fgColor,String savedPath,String content)throws WriterException, IOException {
+        if(savedPath==null){
+            throw new IOException("存储二维码的路径不能为空!");
+        }
+        if(content==null||content.length()<=0){
+            throw new IOException("生成二维码的内容不能为空!");
+        }
         String format = "png";//
+        if(!savedPath.endsWith("."+format)) savedPath += ("."+format);
+
+        if(size<1) size = 1;
+        int width = size; // 图像宽度
+        int height = size; // 图像高度
+
         HashMap<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
         hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        hints.put(EncodeHintType.ERROR_CORRECTION, erroCorrection);
+
         String md5Content = getMD5(content)+"&"+content;
         System.out.println(md5Content);
+
         BitMatrix bitMatrix = new MultiFormatWriter().encode(md5Content,
                 BarcodeFormat.QR_CODE, width, height, hints);// 生成矩阵
-        Path path = FileSystems.getDefault().getPath(PROJECTEC_PATH, content+".png");
-        MatrixToImageWriter.writeToPath(bitMatrix, format, path);// 输出图像
-        System.out.println(new String("输出成功.".getBytes("gbk"),"UTF-8"));
+
+        if((fgColor&0xff000000)==0) fgColor = fgColor&0xff000000;
+        if((bgColor&0xff000000)==0) bgColor = bgColor&0xff000000;
+        MatrixToImageConfig config = new MatrixToImageConfig(fgColor, bgColor);
+
+        Path path = FileSystems.getDefault().getPath(savedPath);
+        MatrixToImageWriter.writeToPath(bitMatrix, format, path,config);// 输出图像
+        System.out.println("输出成功.");
     }
 
     public static String getMD5(String content) {
--- libzxing/src/main/java/com/zxing/util/MyClass.java
+++ libzxing/src/main/java/com/zxing/util/MyClass.java
@@ -1,13 +1,15 @@
 package com.zxing.util;
 
 import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.io.IOException;
 
 public class MyClass {
     public static void main(String[] args){
         try {
-            GenerateQRCode.generatePNG("123456789");
+            GenerateQRCode.generatePNG("123456789");//以默认如此生成的二维码
+            GenerateQRCode.generateQRCODEForParams(ErrorCorrectionLevel.H,50,0xff49070d,0xff000000,"D:/123","123456789");//指定入参生成的二维码
         } catch (WriterException e) {
             e.printStackTrace();
         } catch (IOException e) {
